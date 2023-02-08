08eb62435530974ce1d4192118876e5ada001331
Vaibhav Arora <aroravaibhav817@gmail.com>
2023-02-08 22:42:35 +0530

    Added live page

--- frontend/pages/live/index.tsx
+++ frontend/pages/live/index.tsx
@@ -4,6 +4,18 @@ import styles from "../../styles/Live.module.css";
 
 const Live = () => {
     useEffect(() => {
+        (async function() {
+            console.log('key is', process.env.NEXT_PUBLIC_LIVEPEER_KEY)
+            const data = await fetch('https://livepeer.studio/api/stream/fc7a709b-02ef-4144-b4aa-ee8d93bd0f00', {
+                method: 'GET',
+                headers: {
+                    "Authorization": `Bearer ${process.env.NEXT_PUBLIC_LIVEPEER_KEY}`,
+                }
+            });
+
+            const response = await data.json();
+            console.log(response);
+        })();
 
     }, []);
 
