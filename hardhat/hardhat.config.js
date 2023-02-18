805f3ff84e1b6d7b73efc252a8941d2bb5fe4cf9
Vaibhav Arora <aroravaibhav817@gmail.com>
2023-02-18 11:39:21 +0530

    Changed rpc

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -2,11 +2,13 @@ require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const QUICKNODE_RPC = "https://winter-autumn-gas.matic-testnet.discover.quiknode.pro/23758c0b2ec14cfd8453ca36ee42108792f26a65/";
 module.exports = {
   solidity: "0.8.17",
   networks: {
     mumbai: {
-      url: process.env.MUMBAI_URL,
+      url: QUICKNODE_RPC,
       accounts: [process.env.PRIVATE_KEY]
     }
   }
